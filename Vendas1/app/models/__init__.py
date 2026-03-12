19adcc629138124f3ba50a0a56338ec7d4aaf1a9
Rogerio <rogerio120575@gmail.com>
2026-03-12 09:21:08 -0300

    fix: adicionar pasta static para deploy render

--- Vendas1/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.user import User  # noqa
-from app.models.product import Product  # noqa
-from app.models.order import Order, OrderItem  # noqa
-
-# RBAC
-from app.models.rbac import Role, Permission, UserRole, RolePermission  # noqa
---
+++ static/readme.txt
@@ -0,0 +1,2 @@
+Pasta static do sistema RADAR LOCAL
+Arquivos estáticos: CSS, JS, imagens.
